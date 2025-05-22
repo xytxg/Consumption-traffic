396bd500896a34ec90845a877b2dba5a9109f42c
xytxg <115837765+xytxg@users.noreply.github.com>
2025-05-22 17:27:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ read -p "请输入你的选择（1或2）：" choice
 case $choice in
     1)
         echo "正在下载 '直接24小时持续跑流量' 脚本..."
-        url="https://github.com/net818/Consumption-traffic/raw/main/pp.sh"
+        url="https://github.com/xytxg/Consumption-traffic/blob/main/pp.sh"
         script_file="script.sh"
         wget -O $script_file $url
         chmod +x $script_file
@@ -31,7 +31,7 @@ case $choice in
         ;;
     2)
         echo "正在下载 '随机时间跑流量' 脚本..."
-        url="https://github.com/net818/Consumption-traffic/raw/main/ppp.sh"
+        url="https://github.com/xytxg/Consumption-traffic/blob/main/ppp.sh"
         read -p "请输入最小等待时间（秒）: " min_wait
         read -p "请输入最大等待时间（秒）: " max_wait
         script_file="script.sh"
