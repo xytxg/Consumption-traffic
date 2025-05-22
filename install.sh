e4d1202f2ce993f065da59c66612986b9c3715c8
xytxg <115837765+xytxg@users.noreply.github.com>
2025-05-22 17:38:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [ ! -z "$listen_service" ] && [ "$listen_service" != "nginx" ]; then
 fi
 
 echo "温馨提示!"
-echo "请在脚本执行完后检查 eth0 网卡是否在跑流量"
+echo "请在脚本执行完后检查 enp9s0f1np1 网卡是否在跑流量"
 read -p "请输入已解析本机IP的域名或者本机IP(如果等下流量没在跑就给域名套上CF): " domain_name
 
 echo "请选择安装选项："
@@ -20,7 +20,7 @@ read -p "请输入你的选择（1或2）：" choice
 case $choice in
     1)
         echo "正在下载 '直接24小时持续跑流量' 脚本..."
-        url="https://github.com/xytxg/Consumption-traffic/blob/main/pp.sh"
+        url="https://raw.githubusercontent.com/xytxg/Consumption-traffic/main/pp.sh"
         script_file="script.sh"
         wget -O $script_file $url
         chmod +x $script_file
@@ -29,7 +29,7 @@ case $choice in
         ;;
     2)
         echo "正在下载 '随机时间跑流量' 脚本..."
-        url="https://github.com/xytxg/Consumption-traffic/blob/main/ppp.sh"
+        url="https://raw.githubusercontent.com/xytxg/Consumption-traffic/main/ppp.sh"
         read -p "请输入最小等待时间（秒）: " min_wait
         read -p "请输入最大等待时间（秒）: " max_wait
         script_file="script.sh"
